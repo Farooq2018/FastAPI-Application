0928f4ad736273d9d537b09b328f218f17d7e98f
Farooq2018 <farooqahm97@gmail.com>
2024-02-29 12:20:17 -0600

    Enhanced home.html file and added add-todo functionality to webapp

--- TodoApp/routers/todos.py
+++ TodoApp/routers/todos.py
@@ -1,4 +1,7 @@
-from fastapi import Depends, HTTPException, APIRouter, Request
+from fastapi import Depends, APIRouter, Request, Form
+from sqlalchemy.orm import Session
+from starlette.responses import RedirectResponse
+from starlette import status
 from ..models import Base, Todos
 from ..database import engine, SessionLocal
 from fastapi.responses import HTMLResponse
@@ -24,13 +27,31 @@ def get_db():
 
 
 @router.get("/", response_class=HTMLResponse)
-async def read_all_by_user(request: Request):
-    return templates.TemplateResponse("home.html", {"request": request})
+async def read_all_by_user(request: Request, db: Session = Depends(get_db)):
+    todos = db.query(Todos).filter(Todos.owner_id == 1).all()
+
+    return templates.TemplateResponse("home.html", {"request": request, "todos": todos})
 
 
 @router.get("/add-todo", response_class=HTMLResponse)
 async def add_new_todo(request: Request):
     return templates.TemplateResponse("add-todo.html", {"request": request})
+
+
+@router.post("/add-todo", response_class=HTMLResponse)
+async def create_todo(title: str = Form(...), description: str = Form(...),
+                      priority: int = Form(...), db: Session = Depends(get_db)):
+    todo_model = Todos()
+    todo_model.title = title
+    todo_model.description = description
+    todo_model.priority = priority
+    todo_model.complete = False
+    todo_model.owner_id = 1
+
+    db.add(todo_model)
+    db.commit()
+
+    return RedirectResponse(url="/todos", status_code=status.HTTP_302_FOUND)
 
 
 @router.get("/edit-todo/{todo_id}", response_class=HTMLResponse)
